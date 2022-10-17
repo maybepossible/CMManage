7d925b6bc7891836bc26e20d895a54f5a6bc936e
maybepossible <1944048723@qq.com>
2022-10-17 16:54:12 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,14 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: ['src/views/**/*.vue'],
-      rules: {
-        'vue/multi-word-component-names': 0
-      }
-    }
-  ]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
+    'space-before-function-paren': 0
+  }
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,20 @@
 <template>
-  <h1>首页组件</h1>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
+
 <script>
-export default {}
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
-<style>
+
+<style lang="less" scoped>
+
 </style>
