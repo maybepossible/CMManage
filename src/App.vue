02755e19dde8180aab47f27f548e0647b8ad4ad1
maybepossible <1944048723@qq.com>
2022-10-21 13:29:24 +0800

    完成了权限功能的开发

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-   <!-- 路由占位符 -->
-   <router-view></router-view>
+    <!-- 路由占位符 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app',
-  components: {
-  }
+  components: {}
 }
 </script>
 
@@ -19,7 +17,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
