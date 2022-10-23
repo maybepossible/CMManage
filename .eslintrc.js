ac9eb628caf294e1a4525ca0ae85911f8195c52f
maybepossible <1944048723@qq.com>
2022-10-23 13:05:33 +0800

    完成了功能分类的开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'no-useless-return': 0
   }
 }
