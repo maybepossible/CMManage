a255014b019d574c4457f6fa538c1a6e2b3ff2d7
maybepossible <1944048723@qq.com>
2022-10-30 23:21:35 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,10 +8,17 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 导入axios
 import axios from 'axios'
 // 设置请求根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 设置请求拦截器
 axios.interceptors.request.use(config => {
   // 为请求头对象，添加token验证的Authorization字段
@@ -25,6 +32,19 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
